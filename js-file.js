411ae782bdccfc67bad28806da8a1c3d5e7c8071
Saikat <iamsaikat4@gmail.com>
2023-01-22 21:43:07 +0530

    Add class 'grid' to the div's created by createDiv

--- js-file.js
+++ js-file.js
@@ -4,8 +4,8 @@ const body = document.querySelector('body');
 function createDiv (number) {
     for (let i = 1; i = number*number; i++) {
         const div = document.createElement('div');
+        div.classList.add('grid')
         container.appendChild(div);
-        
     }
     console.log(container.children.length)
 }
