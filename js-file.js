93ce6bda07740ca6ba7ea09551e784d210db9579
Saikat <iamsaikat4@gmail.com>
2023-01-22 22:35:21 +0530

    Fix bug of infinite loop of createDiv function

--- js-file.js
+++ js-file.js
@@ -1,11 +1,13 @@
 const container = document.createElement('div');
 container.classList.toggle('container');
 const body = document.querySelector('body');
+body.appendChild(container);
 function createDiv (number) {
-    for (let i = 1; i = number*number; i++) {
+    for (let i = 1; i <= number*number; i++) {
         const div = document.createElement('div');
         div.classList.add('grid')
         container.appendChild(div);
     }
     console.log(container.children.length)
 }
+createDiv(16);
